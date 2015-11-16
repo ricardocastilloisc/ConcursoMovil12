568cbeb0e82ff9b7eb9486348f83f8113f32ad8f
ricardocastilloisc <ricardocastilloisc@gmail.com>
2015-11-16 12:46:56 -0500

    quitar anuncio

--- resources/views/raza/forms/usr.blade.php
+++ resources/views/raza/forms/usr.blade.php
@@ -2,7 +2,7 @@
   {!!Form::text('nombre',null,['class'=>'form-control', 'placeholder'=>'Ingresa el Nombre de la Raza','style'=>'text-transform:uppercase','onkeyup'=>'javascript:this.value=this.value.toUpperCase();'])!!} </div>
 <div class="form-group">
   <label>Tiempo Aproximado de procucción</label>
-  <label>(Ponga el tiempo si proviende derivado de vacas o hebras)</label>
+
   {!!Form::number('minutos_de_produccion_de_leche',null,['class'=>'form-control', 'placeholder'=>'En minutos','style'=>'text-transform:uppercase','onkeyup'=>'javascript:this.value=this.value.toUpperCase();'])!!} </div>
 <div class="form-group"> {!!Form::label('Cada cuando tiene mes de en celo: ')!!}
   {!!Form::number('dias_de_celo',null,['class'=>'form-control', 'placeholder'=>'En dias','style'=>'text-transform:uppercase','onkeyup'=>'javascript:this.value=this.value.toUpperCase();'])!!} </div>
