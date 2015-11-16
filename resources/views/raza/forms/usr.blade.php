72f26dadada58b618a470ac2998d7112504da2a6
ricardocastilloisc <ricardocastilloisc@gmail.com>
2015-11-16 12:49:18 -0500

    Pequeñas modificaciones

--- resources/views/raza/forms/usr.blade.php
+++ resources/views/raza/forms/usr.blade.php
@@ -4,7 +4,7 @@
   <label>Tiempo Aproximado de procucción</label>
 
   {!!Form::number('minutos_de_produccion_de_leche',null,['class'=>'form-control', 'placeholder'=>'En minutos','style'=>'text-transform:uppercase','onkeyup'=>'javascript:this.value=this.value.toUpperCase();'])!!} </div>
-<div class="form-group"> {!!Form::label('Cada cuando tiene mes de en celo: ')!!}
+<div class="form-group"> {!!Form::label('Cada cuando tiene celo: ')!!}
   {!!Form::number('dias_de_celo',null,['class'=>'form-control', 'placeholder'=>'En dias','style'=>'text-transform:uppercase','onkeyup'=>'javascript:this.value=this.value.toUpperCase();'])!!} </div>
 <div class="form-group"> {!!Form::label('Duración de gestación o eclosión: ')!!}
   {!!Form::number('semanas_de_gestacion',null,['class'=>'form-control', 'placeholder'=>'En semanas','style'=>'text-transform:uppercase','onkeyup'=>'javascript:this.value=this.value.toUpperCase();'])!!} </div>
