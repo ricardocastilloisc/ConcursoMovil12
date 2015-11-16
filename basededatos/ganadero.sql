692d7ef3315e28454621d562420e5f2058f5fa99
ricardocastilloisc <ricardocastilloisc@gmail.com>
2015-11-16 10:16:10 -0500

    Poner la base de datos
    Al no a averiguar sobre notificaciones y avisos pues se pone ya base de
    dato final

--- basededatos/ganadero.sql
+++ basededatos/ganadero.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 16-11-2015 a las 03:05:45
+-- Tiempo de generación: 16-11-2015 a las 08:41:54
 -- Versión del servidor: 10.0.17-MariaDB
 -- Versión de PHP: 5.6.14
 
@@ -40,6 +40,14 @@ CREATE TABLE `animales` (
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Disparadores `animales`
+--
+DELIMITER $$
+CREATE TRIGGER `celonotificacion` AFTER INSERT ON `animales` FOR EACH ROW INSERT INTO notificacionescelo (animal_id, raza_id, tiempo) VALUES (NEW.id, NEW.raza_id, NOW())
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -128,7 +136,24 @@ INSERT INTO `migrations` (`migration`, `batch`) VALUES
 ('2015_11_14_162856_create_crecimientos_table', 1),
 ('2015_11_15_121715_create_destetes_table', 1),
 ('2015_11_15_150126_create_carnes_table', 1),
-('2015_11_15_173903_create_preniars_table', 1);
+('2015_11_15_173903_create_preniars_table', 1),
+('2015_11_16_013535_notificacionescelo', 2),
+('2015_11_16_014758_notificacionescelo', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notificacionescelo`
+--
+
+CREATE TABLE `notificacionescelo` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `animal_id` int(10) UNSIGNED NOT NULL,
+  `raza_id` int(10) UNSIGNED NOT NULL,
+  `tiempo` time NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -172,6 +197,39 @@ CREATE TABLE `razas` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `razas`
+--
+
+INSERT INTO `razas` (`id`, `nombre`, `minutos_de_produccion_de_leche`, `dias_de_celo`, `semanas_de_gestacion`, `created_at`, `updated_at`) VALUES
+(2, 'Vaca Jersey', 4, 1, 17, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, 'Cabra Saanen', 45, 2, 11, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, 'Vaca Suiza', 28, 5, 27, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(8, 'Cerdo  Congo Santadereano', 0, 10, 18, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(9, 'Cerdo Curi', 0, 3, 8, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(10, 'Cerdo Carepalo', 0, 8, 20, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(14, 'Cabra Toggenburg', 8, 9, 13, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(15, 'Cabra Alpina', 10, 2, 19, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(16, 'Cerdo Zungo', 0, 2, 16, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(17, 'Cerdo Sampedreño', 0, 3, 12, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(18, 'Vaca Holstein', 3, 2, 30, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(21, 'Vaca Suiza', 5, 2, 40, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(22, 'Cabra Toggenburg', 5, 2, 17, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(23, 'Cabra Alpina', 4, 2, 20, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(24, 'Oveja Manchega', 0, 2, 17, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(25, 'Oveja Merino', 0, 3, 11, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(28, 'Vaca Holstein', 5, 3, 26, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(29, 'Vaca Suiza', 4, 1, 17, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(30, 'Oveja Merino', 0, 4, 13, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(32, 'Cabra Saanen', 4, 3, 10, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(33, 'Cabra Saanen', 2, 3, 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(34, 'Vaca Jersey', 3, 4, 35, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(37, 'Cerdo Sampedreño', 0, 3, 7, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(38, 'Vaca Holstein', 4, 2, 38, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(39, 'Vaca Holstein', 5, 3, 40, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(40, 'Cerdo Carepalo', 0, 4, 16, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(41, 'Cerdo Sampedreño', 0, 12, 10, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -193,7 +251,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@admin.com', '$2y$10$sy4Q3mwRuWEmJhj6.jJsduC7cFV/vWMq/IOrVhNaupL7No1ZCm2N2', 'XgWOql8ChSUQ7qUtfIOjwQRNBWOSsAwyCOua7xHYLo90Q6KtTg582RhsQJqE', '0000-00-00 00:00:00', '2015-11-16 02:05:22'),
+(1, 'admin', 'admin@admin.com', '$2y$10$sy4Q3mwRuWEmJhj6.jJsduC7cFV/vWMq/IOrVhNaupL7No1ZCm2N2', 'WgnnikDEVXRx8DtX7flwD6lyVoZjmPoc9eZzSwbYkcPoGRTdoW8gpeZbGTte', '0000-00-00 00:00:00', '2015-11-16 02:54:49'),
 (2, 'admin1', 'admin1@admin1.com', '$2y$10$XTyH5zLXLZiqfI0gmGuRLuASqs37tQ6mP359r.3LzMuYxAzXuSpMO', NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 --
@@ -227,6 +285,14 @@ ALTER TABLE `crecimientos`
 ALTER TABLE `destetes`
   ADD PRIMARY KEY (`id`),
   ADD KEY `destetes_animal_id_foreign` (`animal_id`);
+
+--
+-- Indices de la tabla `notificacionescelo`
+--
+ALTER TABLE `notificacionescelo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `notificacionescelo_animal_id_foreign` (`animal_id`),
+  ADD KEY `notificacionescelo_raza_id_foreign` (`raza_id`);
 
 --
 -- Indices de la tabla `password_resets`
@@ -263,12 +329,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `animales`
 --
 ALTER TABLE `animales`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `carnes`
 --
 ALTER TABLE `carnes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `crecimientos`
 --
@@ -278,17 +344,22 @@ ALTER TABLE `crecimientos`
 -- AUTO_INCREMENT de la tabla `destetes`
 --
 ALTER TABLE `destetes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `notificacionescelo`
+--
+ALTER TABLE `notificacionescelo`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `preniars`
 --
 ALTER TABLE `preniars`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `razas`
 --
 ALTER TABLE `razas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
@@ -321,6 +392,13 @@ ALTER TABLE `crecimientos`
 --
 ALTER TABLE `destetes`
   ADD CONSTRAINT `destetes_animal_id_foreign` FOREIGN KEY (`animal_id`) REFERENCES `animales` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `notificacionescelo`
+--
+ALTER TABLE `notificacionescelo`
+  ADD CONSTRAINT `notificacionescelo_animal_id_foreign` FOREIGN KEY (`animal_id`) REFERENCES `animales` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notificacionescelo_raza_id_foreign` FOREIGN KEY (`raza_id`) REFERENCES `razas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `preniars`
