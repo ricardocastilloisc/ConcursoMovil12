3823b5575d3191cbe7896aa1a63de70753ac4661
ricardocastilloisc <ricardocastilloisc@gmail.com>
2015-11-15 21:01:17 -0500

    Se cambio la vista del índex por el tema general
    la razón por la cual se puso todo el tema es porque no averiguamos como
    ocultarlo cuando uno no esta logeado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,47 @@
-@extends('template.template')
-<!-- vista principal de la pagina de usuaurio para poder acceder -->
-@section('content')
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+<title>Control Ganadero</title>
+<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" />
+<!-- FONT AWESOME ICONS  -->
+<link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet" />
+<!-- CUSTOM STYLE  -->
+<link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+<!--La seccion  de lo temos aperye de lo que no estan en el tema pricipl con 
+la finalidad de no saturar de tantos temas la vista principal-->
+
+
+<!-- BOOTSTRAP CORE STYLE  -->
+</head>
+<body>
+
+<!-- HEADER END-->
+<header> </header>
+<!-- /header -->
+<div class="navbar navbar-inverse set-radius-zero">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand" > <img src="{{ asset('img/logo.png') }}" /> </a> </div>
+  </div>
+</div>
+<hr>
+
+<!-- LOGO HEADER END-->
+
+
+<!-- MENU SECTION END-->
+<div class="content-wrapper">
+  <div class="container"> 
+
+
 @include('alerts.errors')
 @include('alerts.request')
 <div class="content-wrapper">
@@ -31,6 +72,32 @@
     </div>
   </div>
 </div>
-@stop
 
 
+
+
+
+
+   </div>
+</div>
+<!-- CONTENT-WRAPPER SECTION END-->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12"> &copy; Castillo Olivera Ricardo Orlando</div>
+      <div class="col-md-12"> &copy; Novelo Sansores Alan Ernesto</div>
+   
+    </div>
+  </div>
+</footer>
+<!-- FOOTER SECTION END--> 
+<!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  --> 
+<!-- CORE JQUERY SCRIPTS --> 
+<script src="{{ asset('js/jquery-1.11.1.js') }}"></script> 
+<!-- BOOTSTRAP SCRIPTS  --> 
+<script src="{{ asset('js/bootstrap.js') }}"></script> 
+<!--los js que no temos y podemos accesar 
+el cuales no saturen al tema principal--> 
+
+</body>
+</html>
