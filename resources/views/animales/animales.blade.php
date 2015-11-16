458362a03024a29da6d5df90a88ee44ab68c1e5e
ricardocastilloisc <ricardocastilloisc@gmail.com>
2015-11-15 22:16:33 -0500

    Solucionar error

--- resources/views/animales/animales.blade.php
+++ resources/views/animales/animales.blade.php
@@ -1,54 +1,68 @@
 <div class="animales">
-<!--visualizamos lo que queremos 
-con las variables se 
-hace otra vista porque 
-ajax o laravel nos pide que declaresmoa tal cual
-lo que quermos 
-visualizar en la paginacion-->
-<div class="row"> @foreach($animales as $animal)
-  <div class="col-md-6"> 
-    <!--   Kitchen Sink -->
-    <div class="panel panel-default">
-      <div class="panel-heading"> <strong>Datos:</strong> </div>
-      <div class="panel-body">
-        <div class="table-responsive">
-          <table class="table table-striped table-bordered table-hover">
-            <tbody>
-              <tr>
-                <th>Nombre:</th>
-              </tr>
-              <tr>
-                <td>{{$animal->nombre}}</td>
-              </tr>
-              <tr>
-                <th>Arete:</th>
-              </tr>
-              <tr>
-                <td>{{$animal->arete}}</td>
-              </tr>
-              <tr>
-                <th>Animal:</th>
-              </tr>
-              <tr>
-                <td>{{$animal->raza}}</td>
-              </tr>
+  <div class="row"> @foreach($animales as $animal)
+    <div class="col-md-6"> 
+      <!--   Kitchen Sink -->
+      <div class="panel panel-default">
+        <div class="panel-heading"> <strong>Datos:</strong> </div>
+        <div class="panel-body">
+          <div class="table-responsive">
+            <table class="table table-striped table-bordered table-hover">
+              <tbody>
+                <tr>
+                  <th>Nombre:</th>
+                </tr>
+                <tr> 
+                  <!--sin tener que explicar mucho solo 
+                accedemos a los valores de la variable-->
+                  <td>{{$animal->nombre}}</td>
+                </tr>
+                <tr>
+                  <th>Arete:</th>
+                </tr>
+                <tr>
+                  <td>{{$animal->arete}}</td>
+                </tr>
+                <tr>
+                  <th>Animal:</th>
+                </tr>
+                <tr>
+                  <td>{{$animal->raza}}</td>
+                </tr>
+                <!--Si no se compro en la granadero no lo muestres-->
+              @if($animal->fecha_de_compra!='0000-00-00')
               <tr>
                 <th>Fecha de Compra:</th>
               </tr>
               <tr>
                 <td>{{$animal->fecha_de_compra}}</td>
               </tr>
+              @endif 
+              <!--Termina--> 
+              <!--Si no se nacio en la granadero no lo muestres--> 
+              @if($animal->fecha_de_nacimiento!='0000-00-00')
               <tr>
                 <th>Fecha de Nacimiento:</th>
               </tr>
               <tr>
                 <td>{{$animal->fecha_de_nacimiento}}</td>
               </tr>
+              @endif 
+              <!--termina--> 
+              <!--si la madre es del granadero entonces ponlo--> 
+              @if($animal->arete_madre!='')
+              <tr>
+                <th>Arete Madre:</th>
+              </tr>
+              <tr>
+                <td>{{$animal->arete_madre}}</td>
+              </tr>
+              @endif 
+              <!--termina-->
               <tr>
                 <th>Peso:</th>
               </tr>
               <tr>
-                <td>{{$animal->peso}}</td>
+                <td>{{$animal->peso}} kg</td>
               </tr>
               <tr>
                 <th>Sexo:</th>
@@ -59,12 +73,13 @@ visualizar en la paginacion-->
               <tr>
                 <td> {!!link_to_route('animal.edit', $title='Editar', $parameters=$animal->id, $attributes=['class'=>'btn btn-primary'])!!} </td>
               </tr>
-            </tbody>
-          </table>
+                </tbody>
+              
+            </table>
+          </div>
         </div>
       </div>
+      <!-- End  Kitchen Sink --> 
     </div>
-    <!-- End  Kitchen Sink --> 
-  </div>
-  @endforeach </div>
-{!!$animales->render()!!}
+    @endforeach </div>
+  {!!$animales->render()!!}
--- resources/views/raza/razas.blade.php
+++ resources/views/raza/razas.blade.php
@@ -1,47 +1,49 @@
-
 <div class="razas">
-<div class="row"> @foreach($razas as $raza)
-  <div class="col-md-6"> 
-    <!--   Kitchen Sink -->
-    <div class="panel panel-default">
-      <div class="panel-heading"> <strong>Datos:</strong> </div>
-      <div class="panel-body">
-        <div class="table-responsive">
-          <table class="table table-striped table-bordered table-hover">
-            <tbody>
-              <tr>
-                <th>Nombre:</th>
-              </tr>
-              <tr>
-                <td>{{$raza->nombre}}</td>
-              </tr>
-              <tr>
-                <th>Aproximación de lactancia:</th>
-              </tr>
-              <tr>
-                <td>{{$raza->minutos_de_produccion_de_leche}} minutos</td>
-              </tr>
-              <tr>
-                <th>Aproximación cuando tiene en celo:</th>
-              </tr>
-              <tr>
-                <td>{{$raza->meses_de_celo}} meses</td>
-              </tr>
-              <tr>
-                <th>Cuanto tiempo tarda la gestación:</th>
-              </tr>
-              <tr>
-                <td>{{$raza->meses_de_gestacion}} meses</td>
-              </tr>
-              <tr>
-                <td> {!!link_to_route('raza.edit', $title='Editar', $parameters=$raza->id, $attributes=['class'=>'btn btn-primary'])!!} </td>
-              </tr>
-            </tbody>
-          </table>
+  <div class="row"> 
+    <!--para poder acceder a la varable como un arreglo--> 
+    @foreach($razas as $raza)
+    <div class="col-md-6"> 
+      <!--   Kitchen Sink -->
+      <div class="panel panel-default">
+        <div class="panel-heading"> <strong>Datos:</strong> </div>
+        <div class="panel-body">
+          <div class="table-responsive">
+            <table class="table table-striped table-bordered table-hover">
+              <tbody>
+                <tr>
+                  <th>Nombre:</th>
+                </tr>
+                <tr>
+                  <td>{{$raza->nombre}}</td>
+                </tr>
+                <tr>
+                  <th>Aproximación de lactancia:</th>
+                </tr>
+                <tr>
+                  <td>{{$raza->minutos_de_produccion_de_leche}} minutos</td>
+                </tr>
+                <tr>
+                  <th>Aproximación cuando tiene en celo:</th>
+                </tr>
+                <tr>
+                  <td>{{$raza->dias_de_celo}} días</td>
+                </tr>
+                <tr>
+                  <th>Cuanto tiempo tarda la gestación o eclosión:</th>
+                </tr>
+                <tr>
+                  <td>{{$raza->semanas_de_gestacion}} semanas</td>
+                </tr>
+                <tr>
+                  <td> {!!link_to_route('raza.edit', $title='Editar', $parameters=$raza->id, $attributes=['class'=>'btn btn-primary'])!!} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
+      <!-- End  Kitchen Sink --> 
     </div>
-    <!-- End  Kitchen Sink --> 
-  </div>
-  @endforeach </div>
-{!!$razas->render()!!}
+    @endforeach </div>
+  <!--es importante para poder conpaginar --> 
+  {!!$razas->render()!!}
